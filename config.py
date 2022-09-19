fefe85e48ad73b72ceaa82288f9c1630ac72da52
CyberKnight1803 <omkarpitale2001@gmail.com>
2022-09-19 13:09:17 +0530

    init

---
+++ config.py
@@ -0,0 +1,12 @@
+import os 
+import torch 
+
+# PATHS 
+PATH_LINCE_DATASET = os.environ.get("./data/lince")
+
+
+# HYPERPARAMS 
+
+
+
+# PROJECT CONFIGURATION
---
+++ main.py
@@ -0,0 +1,15 @@
+import argparse
+from ast import arg 
+
+
+def main(args):
+    pass 
+
+
+if __name__=="__main__":
+    
+    parser = argparse.ArgumentParser()
+
+    args = parser.parse_args()
+
+    main(args)
